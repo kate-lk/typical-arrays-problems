20ca00bec77dacb7e2c017217b89e31463c436a2
Kate Lupashtsian <kate.lupashtsian@gmail.com>
2020-10-02 10:45:31 +0300

    feat: add function max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if (array && array.length > 0) {
+    let min = array[0];
+    for (i=0; i<array.length; i++) {
+      if (array[i] <= min) {
+        min = array[i];
+      } 
+    }
+    return min;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length > 0) {
+    let max = array[0];
+    for (i=0; i<array.length; i++) {
+      if (array[i] >= max) {
+        max = array[i];
+      }
+    }
+    return max;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length > 0) {
+    let avg = 0;
+    let sum = 0;
+    for (i=0; i<array.length; i++) {
+      sum += array[i];
+    }
+    avg = sum / array.length;
+    return avg;
+  } else {
+    return 0;
+  }
 }
